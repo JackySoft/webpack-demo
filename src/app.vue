ec5df0d50dc35555d30ab9e96010f740f9d0c74e
jack <sunnyboysoft@163.com>
2020-04-07 13:08:10 +0800

    编译Vue/React项目

---
+++ src/app.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="app">
+        <h1>欢迎学习webpack4.42知识</h1>
+        <p>{{title}}</p>
+        <p>欢迎关注：前端未来，关乎你的未来</p>
+        <img src="/imgs/qrcode.jpg" alt="">
+    </div>
+</template>
+<script>
+    export default {
+        name:'app',
+        data(){
+            return {
+                title:'Hello Vue'
+            }
+        }
+    }
+</script>  
+<style lang="scss">
+    .app{
+        text-align: center;
+        span{
+            display: inline-block;
+            width:300px;
+            height: 300px;
+            background: url('/imgs/qrcode.jpg') no-repeat;
+            background-size: 300px;
+        }
+    }
+</style>
